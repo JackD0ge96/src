07cc6be0a15e4a9f5f8adaa69408b32e211e5a05
Unknown <marcomurador96@gmail.com>
2017-03-15 16:31:32 +0100

    Singletooon

--- Magic/Gioco.java
+++ Magic/Gioco.java
@@ -2,27 +2,37 @@ package Magic;
 
 import Cards.Omeopathy;
 
-public class Gioco {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Giocatore player1 = new Giocatore("Giocatore1");
-        
-        for (int i = 0; i < 20; i++) {
-            Carta temp = new Omeopathy();
-            player1.riempiMazzo(temp);
+    public class Gioco {   // SINGLETON
+    private Giocatore player1 = new Giocatore("player1");
+    private Giocatore player2 = new Giocatore("player2");
+    
+    private Gioco() {  //COSTRUTTORE PRIVATO
+       
+    }
+    public void prepare(){
+        //riempio mazzo player 1
+        for (int i=0; i<20;i++){
+            Carta temp1=new Omeopathy();
+            player1.riempiMazzo(temp1);     
         }
+        // riempio mazzo player 2
+        for(int i=0; i<20;i++){
+            Carta temp2=new Omeopathy();
+            player2.riempiMazzo(temp2);
+        }
+        player1.pescaInizio();   // pesco 5 carte player 1
+        player2.pescaInizio();   // pesco 5 carte player 2
+        player1.hand.stampaMano();
         
-        player1.pescaInizio();
         
-        while(player1.getHp() > 0){
-            player1.checkScarta();
-            System.out.println(player1.dimensioneMazzo());
-            System.out.println(player1.numeroCarteMano());
-            player1.aggiungiCarta();
-            player1.checkScarta();
-        }    
+       
+    }
+    private static class Instance {
+
+        private static Gioco istanza = new Gioco();
+    }
+
+    public static Gioco getInstance() {
+       return Instance.istanza;
     }
 }
